383e430ee7e9b06c4f4510b6e636a0e808a55182
Krish9006 <guptashanu341@gmail.com>
2026-02-20 23:55:56 +0530

    debug: add secure environment check route

--- server/server.js
+++ server/server.js
@@ -31,6 +31,20 @@ app.get('/', (req, res) => {
     res.send('TrendPulse API is running');
 });
 
+// Diagnostic route for Render config (Securely check if keys exist)
+app.get('/api/debug/env', (req, res) => {
+    res.json({
+        gemini: !!process.env.GEMINI_API_KEY,
+        geminiPrefix: process.env.GEMINI_API_KEY ? process.env.GEMINI_API_KEY.substring(0, 5) : null,
+        news: !!process.env.NEWS_API_KEY,
+        mongo: !!process.env.MONGO_URI,
+        jwt: !!process.env.JWT_SECRET,
+        client: process.env.CLIENT_URL || 'Not Set',
+        nodeEnv: process.env.NODE_ENV || 'Not Set'
+    });
+});
+
+
 // Import Routes
 const taskRoutes = require('./routes/tasks');
 const analysisRoutes = require('./routes/analysis');
