6d7a746c29af277912208bdcaab71b9a8d0c951f
Krish9006 <guptashanu341@gmail.com>
2026-05-03 11:46:04 +0530

    cleanup: remove unused auth routes

--- server/server.js
+++ server/server.js
@@ -75,10 +75,8 @@ app.get('/api/debug/models', async (req, res) => {
 // Import Routes
 const taskRoutes = require('./routes/tasks');
 const analysisRoutes = require('./routes/analysis');
-const authRoutes = require('./routes/auth');
 
 // Use Routes
-app.use('/api/auth', authRoutes);
 app.use('/api/tasks', taskRoutes);
 app.use('/api/analysis', analysisRoutes);
 
